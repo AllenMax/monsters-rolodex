3fa582434e82f3a2f62ad377f534c4fac61e295c
AllenMax <allenm55@hotmail.com>
2020-09-12 20:26:30 -0300

    Updates

---
+++ precache-manifest.a0afd3907e4e43c89690d293b0150ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7bfb7461d8b1b5b18378425f4240b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad78711924a6d610606d",
+    "url": "/monsters-rolodex/static/css/main.efdff999.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad78711924a6d610606d",
+    "url": "/monsters-rolodex/static/js/main.f5d1d80d.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
